e5026e549ff3f7cebd6eebb826c592c44d43b065
emmanuelopeto7-star <emmanuelopeto7@gmail.com>
2026-06-23 09:22:41 +0300

    initialization of repository

--- mongo.py
+++ mongo.py
@@ -3,6 +3,26 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 
 load_dotenv()
-uri="os getenv(mongo_URI)"
+uri=os.getenv("mongo_URI")
 
 client=MongoClient(uri)
+
+db=client["school"]
+
+students = db["students"]
+
+print("Database created successfully")
+# try:
+#     client.admin.command('ping')
+#     print("database has connected")
+# except Exception as e:
+#     print (e)    
+    
+new_student ={
+    "name": "john doe",
+    "age": 20,
+    "grade": "A",
+
+}
+results = students.insert_one(new_student)
+print(f"created student id id {results.inserted_id}")
